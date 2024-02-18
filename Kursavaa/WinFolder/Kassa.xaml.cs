d791f7792f5d131ab1c788cf4e4ae5e0d45ae6d1
Link767 <113935922+Link767@users.noreply.github.com>
2024-02-19 01:37:53 +0300

    Добавьте файлы проекта.

---
+++ Kursavaa/WinFolder/Kassa.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kursavaa.WinFolder
+{
+    public partial class Kassa : Window
+    {
+        public Kassa()
+        {
+            InitializeComponent();
+        }
+
+        private void lk(object sender, RoutedEventArgs e)
+        {
+            LK lK = new LK();
+            lK.Show();
+            this.Close();
+        }
+
+        private void Prod(object sender, RoutedEventArgs e)
+        {
+            Product product = new Product();
+            product.Show();
+            this.Close();
+        }
+
+        private void Zac(object sender, RoutedEventArgs e)
+        {
+            Zac zac = new Zac();
+            zac.Show();
+            this.Close();
+        }
+
+        private void Point(object sender, RoutedEventArgs e)
+        {
+            Point point = new Point();
+            point.Show();
+            this.Close();
+        }
+
+        private void Staf(object sender, RoutedEventArgs e)
+        {
+            Staff staff = new Staff();
+            staff.Show();
+            this.Close();
+        }
+
+        private void User(object sender, RoutedEventArgs e)
+        {
+            Users users = new Users();
+            users.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Kursavaa/WinFolder/Product.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kursavaa.WinFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для Product.xaml
+    /// </summary>
+    public partial class Product : Window
+    {
+        public Product()
+        {
+            InitializeComponent();
+        }
+
+        private void lk(object sender, RoutedEventArgs e)
+        {
+            LK lK = new LK();
+            lK.Show();
+            this.Close();
+        }
+
+        private void Zac(object sender, RoutedEventArgs e)
+        {
+            Zac zac = new Zac();
+            zac.Show();
+            this.Close();
+        }
+
+        private void Point(object sender, RoutedEventArgs e)
+        {
+            Point point = new Point();
+            point.Show();
+            this.Close();
+        }
+
+        private void Staf(object sender, RoutedEventArgs e)
+        {
+            Staff staff = new Staff();
+            staff.Show();
+            this.Close();
+        }
+
+        private void User(object sender, RoutedEventArgs e)
+        {
+            Users users = new Users();
+            users.Show();
+            this.Close();
+        }
+
+        private void Kas(object sender, RoutedEventArgs e)
+        {
+            Kassa kassa = new Kassa();
+            kassa.Show();
+            this.Close();
+        }
+    }
+}
