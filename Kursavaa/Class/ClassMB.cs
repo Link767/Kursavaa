33f957bdf0a06153b46f4e86386ec558c971dbc6
link767 <Linkj767@gmail.com>
2024-04-15 22:59:40 +0300

    Com41

---
+++ Kursavaa/Class/ClassMB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kursavaa.Class
+{
+    internal class ClassMB
+    {
+        public static void ErrorMB(Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public static void ErrorMB(string text)
+        {
+            MessageBox.Show(text, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public static void InformationMB(string text)
+        {
+            MessageBox.Show(text, "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
+
--- Kursavaa/WinFolder/WinEnter.xaml.cs
+++ Kursavaa/WinFolder/WinEnter.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,59 @@ using System.Windows.Shapes;
 namespace Kursavaa.WinFolder
 {
     public partial class WinEnter : Window {
+        SqlConnection sqlConnection = new SqlConnection(App.ConnectionString());
+        SqlCommand sqlCommand;
+        SqlDataReader dataReader;
+        public static int IdRole { get; set; }
         public WinEnter() {
             InitializeComponent();      
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e) {
-            LK lK = new LK();
-            lK.Show();
-            this.Close();
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            {
+                if (string.IsNullOrEmpty(tbLogin.Text))
+                {
+                    Class.ClassMB.InformationMB("Введите логин");
+                }
+                else if (string.IsNullOrEmpty(pbPassword.Password))
+                {
+                    Class.ClassMB.InformationMB("Введите пароль");
+                }
+                else
+                {
+                    try
+                    {
+                        sqlConnection.Open();
+                        sqlCommand = new SqlCommand("Select Password, IdRole from dbo.[Accountant] " +
+                            $"where Login = '{tbLogin.Text}'", sqlConnection);
+                        dataReader = sqlCommand.ExecuteReader();
+                        dataReader.Read();
+
+                        if (pbPassword.Password != dataReader[0].ToString())
+                        {
+                            Class.ClassMB.ErrorMB("Неправильный логин или пароль");
+                        }
+                        else
+                        {
+                            IdRole = Convert.ToInt32(dataReader[1].ToString());
+
+                            switch (IdRole)
+                            {
+                                case 1:
+                                    new LK().Show();
+                                    Close();
+                                    break;
+                            }
+                        }
+                        sqlConnection.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Class.ClassMB.ErrorMB(ex);
+                    }
+                }
+            }
         }
     }
 }
